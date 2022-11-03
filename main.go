b154d3dccbe85cdfee18c814c9d70c6d06933f38
Shreeyash-Naik <shreeyashnaikofficial@gmail.com>
2022-11-03 22:48:48 +0530

    feat: add formfile

--- main.go
+++ main.go
@@ -1,17 +1,30 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 type Payload struct {
 	Name  string `form:"name"`
-	Image []byte `form:"image"`
+	Image string `form:"image"`
 }
 
 func main() {
 	app := fiber.New()
 
-	app.Static("/", "index.html")
+	app.Post("/upload_image", func(ctx *fiber.Ctx) error {
+		file, _ := ctx.FormFile("image")
+
+		ctx.SaveFile(file, "./images/1.png")
+
+		p := new(Payload)
+		ctx.BodyParser(p)
+
+		log.Println("Name: ", p.Name, " Image: ", p.Image)
+		return nil
+	})
+
 	app.Listen(":3000")
 }
