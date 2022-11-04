421da3480a3ba42ad4a6a2cc2127e0ea56a9cddc
Shreeyash-Naik <shreeyashnaikofficial@gmail.com>
2022-11-04 22:34:08 +0530

    feat: parse payload

--- main.go
+++ main.go
@@ -2,28 +2,29 @@ package main
 
 import (
 	"log"
+	"mime/multipart"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 type Payload struct {
-	Name  string `form:"name"`
-	Image string `form:"image"`
+	Name  string                `form:"name"`
+	Image *multipart.FileHeader `form:"image"`
 }
 
 func main() {
 	app := fiber.New()
 
 	app.Post("/upload_image", func(ctx *fiber.Ctx) error {
-		file, _ := ctx.FormFile("image")
-
-		ctx.SaveFile(file, "./images/1.png")
-
 		p := new(Payload)
-		ctx.BodyParser(p)
+		if err := ctx.BodyParser(p); err != nil {
+			log.Println("image upload error --> ", err)
+			return ctx.JSON(fiber.Map{"status": 500, "message": "Server error", "data": nil})
+		}
 
-		log.Println("Name: ", p.Name, " Image: ", p.Image)
-		return nil
+		ctx.SaveFile(p.Image, "./images/1.png")
+
+		return ctx.JSON(fiber.Map{"status": 200, "msg": "Successfully uploaded"})
 	})
 
 	app.Listen(":3000")
