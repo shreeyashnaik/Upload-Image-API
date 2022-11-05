38b3dd2672f5ff64dabbc0f6ec2609183f9e3664
Shreeyash-Naik <shreeyashnaikofficial@gmail.com>
2022-11-05 21:15:06 +0530

    feat: upload image using FormFile method

--- main.go
+++ main.go
@@ -2,27 +2,23 @@ package main
 
 import (
 	"log"
-	"mime/multipart"
 
 	"github.com/gofiber/fiber/v2"
 )
-
-type Payload struct {
-	Name  string                `form:"name"`
-	Image *multipart.FileHeader `form:"image"`
-}
 
 func main() {
 	app := fiber.New()
 
 	app.Post("/upload_image", func(ctx *fiber.Ctx) error {
-		p := new(Payload)
-		if err := ctx.BodyParser(p); err != nil {
+
+		imageFile, err := ctx.FormFile("image")
+
+		if err != nil {
 			log.Println("image upload error --> ", err)
 			return ctx.JSON(fiber.Map{"status": 500, "message": "Server error", "data": nil})
 		}
 
-		ctx.SaveFile(p.Image, "./images/1.png")
+		ctx.SaveFile(imageFile, "./images/my_image.png")
 
 		return ctx.JSON(fiber.Map{"status": 200, "msg": "Successfully uploaded"})
 	})
